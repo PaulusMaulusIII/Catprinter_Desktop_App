62f60f28d89584424a47e1c543d03b0a33f22c80
Paul Schröder <paulschroeder05@outlook.de>
2025-02-06 17:32:26 +0100

    Projektdateien hinzufügen.

---
+++ Base_Window.cs
@@ -0,0 +1,15 @@
+namespace Catprinter_Desktop_App
+{
+    public partial class Base_Window : Form
+    {
+        public Base_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Base_Window_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
