6479840672102091f7d749684065b9ff2e647d5f
Paul Schröder <paulschroeder05@outlook.de>
2025-02-07 17:24:16 +0100

    Desktop App 1.0

--- Base_Window.Designer.cs
+++ Base_Window.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Catprinter_Desktop_App
+﻿namespace Catprinter
 {
     partial class Base_Window
     {
@@ -28,19 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            buttonPrint = new Button();
+            algorithmSelect = new ComboBox();
+            buttonExit = new Button();
+            picturePreview = new PictureBox();
+            pictureBase = new PictureBox();
+            buttonLoadImg = new Button();
+            progressBar1 = new ProgressBar();
+            terminalPanel = new RichTextBox();
+            processButton = new Button();
+            energyField = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)picturePreview).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBase).BeginInit();
             SuspendLayout();
+            // 
+            // buttonPrint
+            // 
+            buttonPrint.Location = new Point(995, 576);
+            buttonPrint.Name = "buttonPrint";
+            buttonPrint.Size = new Size(75, 23);
+            buttonPrint.TabIndex = 1;
+            buttonPrint.Text = "Print\r\n";
+            buttonPrint.UseVisualStyleBackColor = true;
+            buttonPrint.Click += ButtonPrint_Click;
+            // 
+            // algorithmSelect
+            // 
+            algorithmSelect.FormattingEnabled = true;
+            algorithmSelect.Items.AddRange(new object[] { "Floyd-Steinberg", "Halftone", "Atkinson", "Mean-Threshold", "None" });
+            algorithmSelect.Location = new Point(468, 575);
+            algorithmSelect.Name = "algorithmSelect";
+            algorithmSelect.Size = new Size(232, 23);
+            algorithmSelect.TabIndex = 2;
+            algorithmSelect.SelectedIndexChanged += AlgorithmSelect_SelectedIndexChanged;
+            // 
+            // buttonExit
+            // 
+            buttonExit.Location = new Point(12, 576);
+            buttonExit.Name = "buttonExit";
+            buttonExit.Size = new Size(75, 23);
+            buttonExit.TabIndex = 3;
+            buttonExit.Text = "Exit";
+            buttonExit.UseVisualStyleBackColor = true;
+            buttonExit.Click += ButtonExit_Click;
+            // 
+            // picturePreview
+            // 
+            picturePreview.Location = new Point(706, 42);
+            picturePreview.Name = "picturePreview";
+            picturePreview.Size = new Size(364, 527);
+            picturePreview.TabIndex = 5;
+            picturePreview.TabStop = false;
+            // 
+            // pictureBase
+            // 
+            pictureBase.Location = new Point(336, 42);
+            pictureBase.Name = "pictureBase";
+            pictureBase.Size = new Size(364, 528);
+            pictureBase.TabIndex = 6;
+            pictureBase.TabStop = false;
+            pictureBase.Click += PictureBase_Click;
+            // 
+            // buttonLoadImg
+            // 
+            buttonLoadImg.Location = new Point(336, 575);
+            buttonLoadImg.Name = "buttonLoadImg";
+            buttonLoadImg.Size = new Size(126, 23);
+            buttonLoadImg.TabIndex = 7;
+            buttonLoadImg.Text = "Load Image";
+            buttonLoadImg.UseVisualStyleBackColor = true;
+            buttonLoadImg.Click += ButtonLoadImg_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(336, 13);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(734, 23);
+            progressBar1.TabIndex = 8;
+            // 
+            // terminalPanel
+            // 
+            terminalPanel.BackColor = SystemColors.Desktop;
+            terminalPanel.Cursor = Cursors.No;
+            terminalPanel.Font = new Font("Geist Mono Medium", 8.249999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            terminalPanel.ForeColor = SystemColors.HighlightText;
+            terminalPanel.Location = new Point(12, 12);
+            terminalPanel.Name = "terminalPanel";
+            terminalPanel.ReadOnly = true;
+            terminalPanel.ScrollBars = RichTextBoxScrollBars.None;
+            terminalPanel.Size = new Size(318, 557);
+            terminalPanel.TabIndex = 10;
+            terminalPanel.Text = "";
+            // 
+            // processButton
+            // 
+            processButton.Location = new Point(706, 574);
+            processButton.Name = "processButton";
+            processButton.Size = new Size(75, 23);
+            processButton.TabIndex = 11;
+            processButton.Text = "Process";
+            processButton.UseVisualStyleBackColor = true;
+            processButton.Click += processButton_Click;
+            // 
+            // energyField
+            // 
+            energyField.Location = new Point(787, 574);
+            energyField.Name = "energyField";
+            energyField.Size = new Size(202, 23);
+            energyField.TabIndex = 12;
+            energyField.TextChanged += EnergyField_TextChanged;
             // 
             // Base_Window
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1082, 610);
+            Controls.Add(energyField);
+            Controls.Add(processButton);
+            Controls.Add(terminalPanel);
+            Controls.Add(progressBar1);
+            Controls.Add(buttonLoadImg);
+            Controls.Add(pictureBase);
+            Controls.Add(picturePreview);
+            Controls.Add(buttonExit);
+            Controls.Add(algorithmSelect);
+            Controls.Add(buttonPrint);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Base_Window";
-            Text = "Form1";
+            Text = "Catprinter UI";
             Load += Base_Window_Load;
+            ((System.ComponentModel.ISupportInitialize)picturePreview).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBase).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        public Button buttonPrint;
+        public ComboBox algorithmSelect;
+        public Button buttonExit;
+        public PictureBox picturePreview;
+        public PictureBox pictureBase;
+        public Button buttonLoadImg;
+        public ProgressBar progressBar1;
+        public RichTextBox terminalPanel;
+        private Button processButton;
+        private TextBox energyField;
     }
 }
--- Base_Window.cs
+++ Base_Window.cs
@@ -1,7 +1,16 @@
-namespace Catprinter_Desktop_App
+using Catprinter.Utils;
+using InTheHand.Net;
+using InTheHand.Net.Sockets;
+
+namespace Catprinter
 {
     public partial class Base_Window : Form
     {
+        Bitmap img = null;
+        Bitmap processedImg = null;
+        int energy = 0xffff;
+        String dithering = "Floyd-Steinberg";
+
         public Base_Window()
         {
             InitializeComponent();
@@ -9,7 +18,194 @@ namespace Catprinter_Desktop_App
 
         private void Base_Window_Load(object sender, EventArgs e)
         {
-            
+            pictureBase.SizeMode = PictureBoxSizeMode.CenterImage;
+            picturePreview.SizeMode = PictureBoxSizeMode.CenterImage;
+            energyField.Text = "0x" + energy.ToString("X4");
+            algorithmSelect.Text = dithering;
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Base_Window.ActiveForm.Close();
+        }
+
+        private void PictureBase_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => ButtonLoadImg_Click(sender, e));
+        }
+
+        private void EnergyField_TextChanged(object sender, EventArgs e)
+        {
+            if (energyField.Text.Length == 6)
+            {
+                try
+                {
+                    energy = int.Parse(energyField.Text.Remove(0, 2), System.Globalization.NumberStyles.HexNumber);
+                    terminalPanel.AppendText("\nSet Energy to " + energyField.Text);
+                }
+                catch (Exception exc)
+                {
+                    terminalPanel.AppendText("\nNot a valid value");
+                    return;
+                }
+            }
+        }
+
+        private void AlgorithmSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dithering = algorithmSelect.Text;
+            terminalPanel.AppendText("\nSet Dithering to " + dithering);
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => Process());
+        }
+
+        private void ButtonPrint_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => Print());
+        }
+
+        private async void Process()
+        {
+            processedImg = img;
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 10));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nProcessing Image...")));
+            try
+            {
+                processedImg = ImageProcessing.ReadImg("in.jpg", PrinterCommands.PRINT_WIDTH, dithering);
+            }
+            catch (InvalidOperationException exc)
+            {
+                terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\n" + exc.Message)));
+                return;
+            }
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nImage Processed!")));
+            processedImg.Save("out.jpg");
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 90));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nSaved Processed Image as: " + Directory.GetCurrentDirectory() + "\\out.jpg")));
+            SetPicture(picturePreview, processedImg);
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 0));
+        }
+
+        private async void Print()
+        {
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 10));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nFinding Printer")));
+            BluetoothClient client = new BluetoothClient();
+            IReadOnlyCollection<BluetoothDeviceInfo> devices = client.DiscoverDevices();
+            BluetoothDeviceInfo printer = null;
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                if (device.DeviceName == "X5h-0000")
+                {
+                    printer = device;
+                    break;
+                }
+            }
+            if (printer == null)
+            {
+                terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nPrinter not found")));
+                return;
+            }
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nPrinter found")));
+            BluetoothAddress address = printer.DeviceAddress;
+            IReadOnlyCollection<Guid> services = printer.InstalledServices;
+            Guid spp = new Guid();
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 20));
+
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nName: " + printer.DeviceName)));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nAddress: " + address)));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nServices:")));
+            foreach (Guid service in services)
+            {
+                terminalPanel.Invoke((Action)(() => terminalPanel.AppendText(" - " + service.ToString().ToUpper())));
+                if (service.ToString().ToUpper().Equals("00001101-0000-1000-8000-00805F9B34FB"))
+                {
+                    spp = service;
+                    terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\n    -> SPP service found")));
+                    break;
+                }
+            }
+            if (!spp.ToString().ToUpper().Equals("00001101-0000-1000-8000-00805F9B34FB"))
+            {
+                terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nSPP service not found")));
+                return;
+            }
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 30));
+
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nConnecting to printer")));
+            try
+            {
+                client.Connect(printer.DeviceAddress, spp);
+            }
+            catch (Exception err)
+            {
+                terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nFailed to connect")));
+                return;
+            }
+            Stream stream = client.GetStream();
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nConnected to printer")));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nGenerating and sending commands")));
+            stream.Write(PrinterCommands.GetImgPrintCmd(processedImg, energy));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nCommands sent")));
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 90));
+
+            stream.Close();
+            client.Close();
+
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nConnection closed")));
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 0));
+        }
+        private async void ButtonLoadImg_Click(object sender, EventArgs e)
+        {
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 0));
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 10));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nLoading Image...")));
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 90));
+            string selectedFileName = openFileDialog.FileName;
+            img = new Bitmap(selectedFileName);
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 100));
+            Thread.Sleep(500);
+            img.Save("in.jpg");
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nImage Loaded!")));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nImage Path: " + selectedFileName)));
+            terminalPanel.Invoke((Action)(() => terminalPanel.AppendText("\nSaved Image as: " + Directory.GetCurrentDirectory() + "\\in.jpg")));
+            SetPicture(pictureBase, img);
+
+            progressBar1.Invoke((Action)(() => progressBar1.Value = 0));
+        }
+
+        private void SetPicture(PictureBox pictureBox, Bitmap img)
+        {
+            int width = img.Width;
+            int height = img.Height;
+            double factor = 1;
+            Bitmap resized = img;
+            if (height > width && height > pictureBox.Height)
+            {
+                factor = (double)pictureBox.Height / height;
+                resized = new Bitmap(img, new Size((int)(width * factor), (int)(height * factor)));
+            }
+            else if (width > height && width > pictureBox.Width)
+            {
+                factor = (double)pictureBox.Width / width;
+                resized = new Bitmap(img, new Size((int)(width * factor), (int)(height * factor)));
+            }
+            pictureBox.Image = resized;
         }
     }
 }
